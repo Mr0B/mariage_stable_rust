f2eb86d0503f6167c70f598b1a2f66081048814d
Mr0B <luca.orengo@etu.univ-cotedazur.fr>
2021-10-17 23:05:44 +0200

    -added basic Man and Woman struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use crate::test_object::{Man, Woman};
+
+mod test_object;
+
 fn main() {
-    println!("Hello, world!");
+    let mut test_man = Man{
+        0: 0,
+        1: vec![2, 0, 1],
+        2: -1
+    };
+    let mut _test_woman = Woman{
+        0: 2,
+        1: vec![1, 0, 2],
+        2: -1
+    };
+    println!("{}", test_man.0);
+    println!("{}", test_man.2);
+    test_man.2=2;
+    println!("{}", test_man.2);
 }
---
+++ src/test_object.rs
@@ -0,0 +1,3 @@
+pub struct Man(pub i32, pub Vec<i32>, pub i32,);
+
+pub struct Woman(pub i32, pub Vec<i32>, pub i32,);
