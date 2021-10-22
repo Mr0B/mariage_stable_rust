8a81b83a7a38abf214197bfdc6960849f41f259e
Mr0B <luca.orengobrando@gmail.com>
2021-10-22 11:31:27 +0200

    -Added proper constructor for Man and Woman. -Added check_favorite method for Woman.

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,12 @@ use crate::test_object::{Man, Woman};
 mod test_object;
 
 fn main() {
-    let mut test_man = Man{
-        0: 0,
-        1: vec![2, 0, 1],
-        2: -1
-    };
-    let mut _test_woman = Woman{
-        0: 2,
-        1: vec![1, 0, 2],
-        2: -1
-    };
-    println!("{}", test_man.0);
-    println!("{}", test_man.2);
-    test_man.2=2;
-    println!("{}", test_man.2);
+    let test_man: Man = test_object::Man::new(0, vec![2, 0, 1],-1);
+    let test_man2: Man = test_object::Man::new(1, vec![0, 2, 1],-1);
+    let mut test_woman: Woman = test_object::Woman::new(2, vec![1, 0, 2], -1);
+    println!("{}", test_woman.2);
+    let _dumped: i32 = test_woman.check_favorite(test_man);
+    println!("{}", test_woman.2);
+    let _dumped2: i32 = test_woman.check_favorite(test_man2);
+    println!("{}", test_woman.2);
 }
--- src/test_object.rs
+++ src/test_object.rs
@@ -1,3 +1,27 @@
 pub struct Man(pub i32, pub Vec<i32>, pub i32,);
 
+impl Man{
+    pub(crate) fn new(name: i32, preference: Vec<i32>, proposing_to: i32) -> Man {
+        Man { 0: name, 1: preference, 2: proposing_to }
+    }
+}
+
+//favorite is public only for testing purposes
 pub struct Woman(pub i32, pub Vec<i32>, pub i32,);
+
+impl Woman{
+    pub(crate) fn new(name: i32, preference: Vec<i32>, favorite: i32) -> Woman {
+        Woman { 0: name, 1: preference, 2: favorite }
+    }
+    pub(crate) fn check_favorite(&mut self, pretending: Man) -> i32 {
+        let position_pretending =self.1.iter().position(|&r| r == pretending.0).unwrap();
+        let position_favorite=self.1.iter().position(|&r| r == self.2).unwrap_or(999999999);
+        return if position_favorite > position_pretending {
+            let former_favorite = self.2;
+            self.2 = pretending.0;
+            former_favorite
+        } else {
+            pretending.0
+        }
+    }
+}
