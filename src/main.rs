da5f44ed3d2efd790b68359fcf0159d6fcaa0b0b
Mr0B <luca.orengobrando@gmail.com>
2021-10-22 15:49:03 +0200

    -Fix bug linked to call by reference.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 mod object;
 
+use rand::prelude::SliceRandom;
 use crate::object::{deck, man, woman};
 use crate::object::deck::*;
 use crate::object::man::*;
 use crate::object::woman::*;
+use rand::Rng;
 
 fn main() {
-    let mut _test_deck: Deck = deck::Deck::new(vec![]);
+    let mut test_deck: Deck = deck::Deck::new(vec![]);
     let mut test_woman: Woman = woman::Woman::new(2, vec![1, 0, 2], -1);
 
-    println!("{}", test_woman.0)
+    init_men(&mut test_deck, 5);
+    for i in 0..5 {
+        let holder =test_deck.give_first();
+        match holder{
+            Some(man) => println!("{}{:?}{}",man.0, man.1, man.2),
+            None => println!("didn't work"),
+        }
+    }
 }
 
-fn init_men(mut deck:Deck, number:i32) {
+fn init_men(deck: &mut Deck, number:i32) {
     for i in 0..number {
-        let man_holder = man::Man::new(i, generate_man_preference(number, i), -1);
+        let man_holder = man::Man::new(i, generate_preference(number, i), -1);
         deck.put_at_the_end(man_holder);
     }
 }
 
 fn generate_preference(size: i32, preference_of: i32) -> Vec<i32> {
-    let mut my_vector : Vec<i32> = (1..size+1).map(|x| x).collect();
-    my_vector.retain(|x| *x != preference_of);
-    rand::thread_rng().shuffle(my_vector)
+    let mut my_vector : Vec<i32> = (0..size+1).map(|x| x).collect();
+    let mut rng = rand::thread_rng();
+    my_vector.shuffle(&mut rng);
+    my_vector
 }
--- src/object/deck.rs
+++ src/object/deck.rs
@@ -7,17 +7,11 @@ impl Deck{
         Deck { 0: men}
     }
 
-    pub(crate) unsafe fn give(&self) -> &Man {
-        let first: &Man = self.0.get_unchecked(0);
-        return first
+    pub(crate) fn give_first(&mut self) -> Option<Man> {
+        self.0.pop()
     }
 
     pub(crate) fn put_at_the_end(&mut self, newly_single: Man) {
-        &self.0.push(newly_single);
-    }
-
-    pub(crate) fn is_empty() -> bool {
-        let check: bool = self::deck.0.is_empty();
-        return check
+        self.0.push(newly_single);
     }
 }
