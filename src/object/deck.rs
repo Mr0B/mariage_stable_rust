f07c457536d6a60e7cefb5cae2bf0865d7d579be
Mr0B <luca.orengo@etu.univ-cotedazur.fr>
2021-11-05 09:56:13 +0100

    -removed shorthand inside woman struct.

--- src/object/deck.rs
+++ src/object/deck.rs
@@ -1,21 +1,23 @@
 use crate::man::Man;
 
-pub struct Deck(pub Vec<Man>);
+pub struct Deck {
+    pub men: Vec<Man>,
+}
 
 impl Deck{
     pub(crate) fn new(men: Vec<Man>) -> Deck {
-        Deck { 0: men}
+        Deck { men }
     }
 
     pub(crate) fn give_first(&mut self) -> Option<Man> {
-        self.0.pop()
+        self.men.pop()
     }
 
     pub(crate) fn put_at_the_end(&mut self, newly_single: Man) {
-        self.0.push(newly_single);
+        self.men.push(newly_single);
     }
 
     pub(crate) fn empty(&mut self) -> bool {
-        return self.0.is_empty();
+        return self.men.is_empty();
     }
 }
