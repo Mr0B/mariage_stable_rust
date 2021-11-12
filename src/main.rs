c5dae5978f63b442150cbe39723943340411e51c
Mr0B <luca.orengobrando@gmail.com>
2021-11-12 14:36:45 +0100

    -Turned Deck into a trait instead of a struct.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ mod test;
 
 use std::collections::HashMap;
 use rand::prelude::SliceRandom;
-use crate::object::{deck, man, woman};
+use crate::object::{man, woman};
 use crate::object::deck::*;
 use crate::object::woman::*;
 
 fn main() {
-    let mut test_deck: Deck = deck::Deck::new(vec![]);
+    let mut test_deck: Storage = Deck::new(vec![]);
     init_men(&mut test_deck, 5);
     let mut test_women: HashMap<i32, Woman> = init_woman(5);
-    mariage_stable(&mut test_deck, &mut test_women);
+    marriage_stable(&mut test_deck, &mut test_women);
 }
 
-fn mariage_stable(deck: &mut Deck, women: &mut HashMap<i32, Woman>) {
-    while let Some(mut man_proposing) = deck.give_first() {
+fn marriage_stable(deck: &mut Storage, women: &mut HashMap<i32, Woman>) {
+    while let Some(mut man_proposing) = deck.pop() {
         if let Some(target) = man_proposing.find_next_woman() {
             let woman_being_proposed_to: Option<&mut Woman> = women.get_mut(target);
             match woman_being_proposed_to {
@@ -23,7 +23,7 @@ fn mariage_stable(deck: &mut Deck, women: &mut HashMap<i32, Woman>) {
                 Some(woman) => {
                     let dropped_man = woman.check_favorite(man_proposing);
                     if dropped_man.name != -1 {
-                        deck.put_at_the_end(dropped_man);
+                        deck.add(dropped_man);
                     }
                 }
             }
@@ -31,10 +31,10 @@ fn mariage_stable(deck: &mut Deck, women: &mut HashMap<i32, Woman>) {
     }
 }
 
-fn init_men(deck: &mut Deck, number: i32) {
+fn init_men(deck: &mut Storage, number: i32) {
     for i in 0..number {
         let man_holder = man::Man::new(i, generate_preference(number), -1);
-        deck.put_at_the_end(man_holder);
+        deck.add(man_holder);
     }
 }
 
--- src/object/deck.rs
+++ src/object/deck.rs
@@ -1,19 +1,25 @@
 use crate::man::Man;
 
-pub struct Deck {
-    pub men: Vec<Man>,
+pub(crate) struct Storage {men: Vec<Man>}
+
+pub (crate) trait Deck {
+    fn new(men: Vec<Man>) -> Self;
+
+    fn add(&mut self, newly_single: Man);
+
+    fn pop(&mut self) -> Option<Man>;
 }
 
-impl Deck{
-    pub(crate) fn new(men: Vec<Man>) -> Deck {
-        Deck { men }
+impl Deck for Storage {
+    fn new(men: Vec<Man>) -> Storage {
+        Storage {men}
     }
 
-    pub(crate) fn give_first(&mut self) -> Option<Man> {
-        self.men.pop()
-    }
-
-    pub(crate) fn put_at_the_end(&mut self, newly_single: Man) {
+    fn add(&mut self, newly_single: Man) {
         self.men.push(newly_single);
+    }
+
+    fn pop(&mut self) -> Option<Man> {
+        self.men.pop()
     }
 }
